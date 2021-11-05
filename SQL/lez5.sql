e832942343343d33d52854110450bceea4debeac
LucaTaro <91669310+LucaTaro@users.noreply.github.com>
2021-11-05 14:52:55 +0100

    c

--- SQL/lez5.sql
+++ SQL/lez5.sql
@@ -1,1 +1,8 @@
-select AVG(prezzo) AS "prezzo medio" from libro; 
+select AVG(prezzo) AS "prezzo medio" from libro;
+select count(*) from studente; --conta le righe
+select max(prezzo), min(prezzo) from libro;
+select floor(prezzo), ceiling (prezzo) from libro;
+select round(nomecampo);--arrotondamento
+select nome, length(nome) 
+from studente; --lunghezza dei caratteri di ciascuno
+
